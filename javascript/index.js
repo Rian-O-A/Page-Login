a1593600764dbcce7efd387d1ad8a3b800ae43ef
Rian Almeida <rianoliveira38@gmail.com>
2023-07-19 11:12:21 -0300

    Adição de cookies na page de login

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,87 @@
-function passwordVisibility(){
+function passwordVisibility(x){
     
-    let passwordInput = document.getElementById("password")
-    let eyesLook = document.getElementById('toggle-password-icon')
+    let passwordInput = document.querySelectorAll(".passwdInput")
+    let eyesLook = document.querySelectorAll(".iconEyes")
 
-    if(passwordInput.type ==='password'){
-        passwordInput.type = 'text'
-        eyesLook.classList.add('fa-eye')
-        eyesLook.classList.remove('fa-eye-slash')
+    
+        
+    if(passwordInput[x].type ==='password'){
+        passwordInput[x].type = 'text'
+        eyesLook[x].classList.add('fa-eye')
+        eyesLook[x].classList.remove('fa-eye-slash')
     
 
     }else{
-        passwordInput.type = 'password'
-        eyesLook.classList.add('fa-eye-slash')
-        eyesLook.classList.remove('fa-eye')
-   
+        passwordInput[x].type = 'password'
+        eyesLook[x].classList.add('fa-eye-slash')
+        eyesLook[x].classList.remove('fa-eye')
+    
     }
+    
 
 }
 
+function remenberMe(){
+    let salvarObject = {email: document.querySelector('#userEmail').value, password: document.querySelector('#password').value}
+    const dataExpiracao = new Date('9999-12-31').toUTCString()
+    const cookie = `inputs=${encodeURIComponent(JSON.stringify(salvarObject))}; expires=${dataExpiracao}; path=/`
+    document.cookie = cookie
+}
 
-var togglePasswordIcon = document.getElementById('toggle-password-icon');
-var togglePassword = document.getElementById('toggle-password');
-togglePassword.addEventListener('click', passwordVisibility);
+function getObjectFromCookie(nome) {
+    const cookies = document.cookie.split(';')
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim()
+      if (cookie.startsWith(nome + '=')) {
+        const objetoCodificado = cookie.substring(nome.length + 1)
+        const objetoDecodificado = decodeURIComponent(objetoCodificado)
+        return JSON.parse(objetoDecodificado)
+      }
+    }
+    return null
+  }
 
+function deleteCookie() {
+    document.cookie = `inputs=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+}
+
+
+  
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const objetoSalvo = getObjectFromCookie('inputs')
+    if (objetoSalvo){
+    
+        document.querySelector("#remember").checked = true
+        document.querySelector("#password").value = objetoSalvo.password
+        document.querySelector("#userEmail").value = objetoSalvo.email
+    }
+})
+
+document.querySelector("#remember").addEventListener('change', function() {
+    if (document.querySelector("#remember").checked) {
+      remenberMe()
+    } else {
+      deleteCookie()
+    }})
+
+
+
+
+var togglePassword = document.getElementById('toggle-password')
+var togglePasswordVerify = document.getElementById('toggle-passwordVerify')
+togglePassword.addEventListener('click', function () {
+    passwordVisibility(0)})
+if(togglePasswordVerify){
+
+    togglePasswordVerify.addEventListener('click',  function () {
+        passwordVisibility(1)})
+}
 
 
 var video = document.getElementById('video-background')
-video.style.display = ''
+if (video){
+    video.style.display = ''
+}
+
